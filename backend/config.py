7dbd645a40c42a24cca902e396ebcb35f9dcb7b2
Gnomski <gritzzz@inbox.ru>
2025-06-09 21:54:47 +0300

    model

--- backend/config.py
+++ backend/config.py
@@ -13,8 +13,8 @@ SERPER_API_KEY = os.getenv("SERPER_API_KEY")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Модель по умолчанию
-GROQ_MODEL = "llama-3.3-70b-versatile"
-# GROQ_MODEL = "llama-3.1-8b-instant"
+# GROQ_MODEL = "llama-3.3-70b-versatile"
+GROQ_MODEL = "meta-llama/llama-guard-4-12b"
 
 # Системный промпт
 SYSTEM_PROMPT = """
