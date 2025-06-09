a33575fa16c04c5d8fb4c859c6c57ad7ae9786d8
Gnomski <gritzzz@inbox.ru>
2025-06-09 22:04:07 +0300

    time

--- backend/services/functions.py
+++ backend/services/functions.py
@@ -78,7 +78,7 @@ async def send_reminder_timer(user_id: int, time_str: str, medicine: str):
     tz = pytz.timezone("Europe/Moscow")
     now = datetime.now(tz)
     target_time = datetime.strptime(time_str, "%H:%M").replace(
-        year=now.year, month=now.month, day=now.day
+        year=now.year, month=now.month, day=now.day, tzinfo=moscow
     )
 
     delay = (target_time - now).total_seconds()
